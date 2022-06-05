6379a711c4a0be6c01e51d5329ebed12a92e9431
Maksim Yavlyanskiy <max_yav@csltd.com.ua>
2022-06-05 18:17:49 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text:string
+  id?:number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course';
+  posts: Post[] = [
+    {
+      title:'Letn components',
+      text: 'loren10',
+      id:1
+    },
+    {
+      title:'Letn components 2',
+      text: 'loren20',
+      id:2
+    }
+  ]
+
+  updatePosts(post:Post) {
+    this.posts.unshift(post)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostFormComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
